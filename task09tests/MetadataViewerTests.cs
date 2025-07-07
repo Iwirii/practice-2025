77b4c34e9d3edfacd0c6a59f7ce4e20ab47cbbda
Ksenia_Firstova <ksu0655@mail.ru>
2025-07-07 14:39:56 +0600

    feat(task09): add console app to display DLL metadata

---
+++ task09tests/MetadataViewerTests.cs
@@ -0,0 +1,31 @@
+using System;
+using Xunit;
+using task09;
+
+namespace task09tests
+{
+    public class MetadataViewerTests
+    {
+        [Fact]
+        public void Main_ShowsError_WhenNoArguments()
+        {
+            MetadataViewer.Main(Array.Empty<string>());
+        }
+
+        [Fact] 
+        public void Main_ShowsError_WhenFileMissing()
+        {
+            MetadataViewer.Main(new[] { "missing.dll" });
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Main_RunsWithoutErrors_WithTestAssembly()
+        {
+            var testDll = typeof(MetadataViewerTests).Assembly.Location;
+            
+            MetadataViewer.Main(new[] { testDll });
+            Assert.True(true);
+        }
+    }
+}
