0b57b4230c65389101608f3a9cda1661f424940b
Ksenia_Firstova <ksu0655@mail.ru>
2025-07-17 19:18:19 +0600

    feat: optimize and benchmark multithreaded definite integral calculation

--- task14/DefiniteIntegral.cs
+++ task14/DefiniteIntegral.cs
@@ -1,35 +1,38 @@
-﻿using System;
-using System.Threading;
-namespace task14;
-
+﻿namespace task14;
 public static class DefiniteIntegral
 {
     public static double Solve(double a, double b, Func<double, double> function, double step, int threadsNumber)
     {
-        double total = 0.0;
-        using var barrier = new Barrier(threadsNumber + 1);
-        double segment = (b - a) / threadsNumber;
+        double[] localSums = new double[threadsNumber];
+        double chunkSize = (b - a) / threadsNumber;
 
-        for (int i = 0; i < threadsNumber; i++)
+        Parallel.For(0, threadsNumber, i =>
         {
-            double start = a + i * segment;
-            double end = (i == threadsNumber - 1) ? b : start + segment;
-
-            new Thread(() =>
+            double start = a + i * chunkSize;
+            double end = (i == threadsNumber - 1) ? b : start + chunkSize;
+            double sum = 0.0;
+            for (double x = start; x < end; x += step)
             {
-                double partialSum = 0;
-                for (double x = start; x < end; x += step)
-                {
-                    double currentStep = Math.Min(step, end - x);
-                    partialSum += (function(x) + function(x + currentStep)) * currentStep / 2;
-                }
+                double dx = Math.Min(step, end - x);
+                sum += (function(x) + function(x + dx)) * dx / 2;
+            }
+            localSums[i] = sum;
+        });
 
-                Interlocked.Exchange(ref total, total + partialSum);
-                barrier.SignalAndWait();
-            }).Start();
-        }
-
-        barrier.SignalAndWait();
+        double total = 0;
+        for (int i = 0; i < threadsNumber; i++)
+            total += localSums[i];
         return total;
+    }
+
+    public static double SolveSingleThread(double a, double b, Func<double, double> function, double step)
+    {
+        double sum = 0.0;
+        for (double x = a; x < b; x += step)
+        {
+            double dx = Math.Min(step, b - x);
+            sum += (function(x) + function(x + dx)) * dx / 2;
+        }
+        return sum;
     }
 }
